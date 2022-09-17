4854fe57c8d16f3c7d710dcef3c8a72183f52fba
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-17 22:43:18 +0800

    Time: 1 ms, Memory: 41.6 MB - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
+        int left = 0;
+        int right = x/2+1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid > x/mid) {
+                right = mid -1;
+            }
+            else if (mid < x/mid) {
+                left = mid + 1;
+                if (left > x/left) {
+                    return mid;
+                }
+            }
+            else { 
+                return mid;
+            }
+        }
+        return left;
+    }
+}
