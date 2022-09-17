6ed5459814f005d8bfce2284e5a6ea59474a9b27
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-17 15:05:24 +0800

    Time: 23 ms (5.39%), Space: 42.1 MB (7.67%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) return true;
+        int left = 0;
+        int right = num / 2 + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            System.out.println(left +", "+right+", "+mid);
+            if (mid > num*1.0 / mid) {
+                right = mid - 1;
+            }
+            else if (mid < num*1.0 / mid) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
