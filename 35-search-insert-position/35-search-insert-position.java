5e3286903eacee861dc8bff026550da631492e67
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-20 00:09:48 +0800

    Time: 0 ms (100.00%), Space: 43.5 MB (42.22%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,10 +1,29 @@
 class Solution {
+public int searchInsert(int[] nums, int target) {
+        int left = 0 ;         
+        int right = nums.length;
+    
+        while (left < right) {            
+            int mid = left + (right-left)/2;            
+            if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else right = mid;
+        }
+        return left;
+    }
+}
+
+/*
+class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         
         while (left <= right) {
             int mid = (left + right) /2;
+            //System.out.println("left: "+left +", right: "+right+", mid: "+mid);
+            
             if (nums[mid] > target) {
                 right = mid - 1;
             } else if (nums[mid] < target) {
@@ -16,3 +35,4 @@ class Solution {
         return left;
     }
 }
+*/
