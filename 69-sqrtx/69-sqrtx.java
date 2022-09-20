2a53be6cf323a22d35346a5e9b5c45fc2b1d6412
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-20 22:55:50 +0800

    Time: 2 ms (84.27%), Space: 41.5 MB (34.96%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,8 +1,33 @@
 class Solution {
     public int mySqrt(int x) {
         if (x == 0 || x == 1) return x;
+        int left = 1;
+        int right = x;
+        
+        while (left < right) {
+            int mid = left + (right - left+1) / 2;
+            if (mid > x/mid) {
+                right = mid-1 ;
+            }
+            else if (mid < x/mid) {
+                left = mid;
+               
+            }
+            else { 
+                return mid;
+            }
+        }
+        return left;
+    }
+}
+
+/*
+// Left closed, right closed
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) return x;
         int left = 0;
-        int right = x/2 + 1;
+        int right = x/2+1;
         
         while (left <= right) {
             int mid = left + (right - left) / 2;
@@ -22,3 +47,4 @@ class Solution {
         return left;
     }
 }
+*/
