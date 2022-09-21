bbed1aa3faa5d1ff1fc561fc2476c913bcdf63f9
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-21 22:16:19 +0800

    Time: 0 ms (100.00%), Space: 47.3 MB (50.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,5 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        //Left Closed - Right Open
+        int left = 0; 
+        int right = nums.length;
+        int first = -1;
+        int last = -1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }  else {
+                first = mid;
+                right = mid ;
+            }
+        }
+        left = first;
+        if (left != -1) {
+            right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > target) {
+                    right = mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                }  else {
+                    last = mid;
+                    left = mid + 1;
+                }
+            }
+        }
+        int[] result = {first, last};
+        return result;
+    }
+}
+/*
+//Left Closed - Right Closed
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
         int left = 0; 
         int right = nums.length - 1;
         int first = -1;
@@ -33,4 +72,4 @@ class Solution {
         int[] result = {first, last};
         return result;
     }
-}
+}*/
