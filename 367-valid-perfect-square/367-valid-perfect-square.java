4d354cccb339718d8a65575838fbf17555a8351d
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-22 23:22:18 +0800

    Time: 3 ms (7.54%), Space: 38.8 MB (98.73%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.java
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -1,6 +1,27 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         if (num == 1) return true;
+        int left = 1; 
+        int right = num;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mid > num*1.0 / mid) {
+                right = mid;            
+            } else if (mid < num*1.0/mid){
+                left = left + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/*
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        //Left Closed - Right Closed
+        if (num == 1) return true;
         int left = 0;
         int right = num / 2;
         while (left <= right) {
@@ -18,3 +39,4 @@ class Solution {
         return false;
     }
 }
+*/
