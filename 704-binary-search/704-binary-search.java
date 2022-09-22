63cb7d27d282115aae9d97d96d3b151e9603f97c
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-22 22:25:05 +0800

    Time: 0 ms (100.00%), Space: 54.1 MB (60.32%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,42 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // Left closed and right open interval
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target) {
-                right = mid;
-            } else if (nums[mid] < target) {
-                left = mid +1;
-            } else {
-                return mid;
-            }
-        }
-        if(nums[left] == target) 
-             return left;
-        return -1;
-    }
-}
-/*
-// Left closed and right closed interval
-class Solution {
-    public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
-        
         while (left < right) {
-            int mid = (left + right) / 2;
-            System.out.println("left: "+left +", right: "+right+", mid: "+mid);
+            int mid = left + (right-left)/2;
             if (nums[mid] > target) {
                 right = mid;
-            } else  if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+            } else if ( nums[mid] < target) {
+                left = mid +1;
+            }
+            else {
                 return mid;
             }
         }
         return -1;
     }
 }
-*/
+    
