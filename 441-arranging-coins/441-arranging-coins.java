947a7ed6c0030fc611c7e69922cca4c83a4f0eb7
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-24 17:01:27 +0800

    Time: 1 ms (100.00%), Space: 39.5 MB (96.17%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -3,15 +3,16 @@ class Solution {
         if (n == 1) return 1;
         int left = 1;
         int right = n;
-        while ( left < right ) {
-            int mid = left + (right - left) / 2;
-            long sum = (long)((mid+1)*(mid*1.0/2));
-            //System.out.println(left+","+right+","+mid+","+sum);
+        while (left < right) {
+            int mid = left + (right -left)/2;
+            long sum = (long)(mid * ((1 +mid*1.0)/2));
+            //System.out.printf("left: %d, right: %d, mid: %d, sum: %d%n", left,right,mid,sum);
             if (sum > n) {
                 right = mid;
-            } if (sum <= n) {
+            } else
+            if (sum <= n) {
                 left = mid + 1;
-            }
+            } 
         }
         return left-1;
     }
