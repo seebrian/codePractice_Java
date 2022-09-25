74764f06564ae886f5ad83c7c5e2698e486c47f3
Brian See <31948672+seebrian@users.noreply.github.com>
2022-09-25 10:11:02 +0800

    Time: 11 ms (10.84%), Space: 50.2 MB (43.27%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,28 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        //Two pointers
+        int left = 0;
+        int right = numbers.length-1;
+        int[] result = new int[2];
+        while (left < right) {
+            int total = numbers[left] + numbers[right];
+            if (total == target) {
+                result[0] = left + 1;
+                result[1] = right + 1; 
+                return result;
+            } else if (total > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return result;
+    }
+}
+/*
+//binary search
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
         for (int i = 0; i< numbers.length; i++) {
             int left = i;
             int right = numbers.length;
@@ -21,3 +44,4 @@ class Solution {
         return null;
     }
 }
+*/
