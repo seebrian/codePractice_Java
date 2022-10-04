980cf243e77acfceaf472499b25e9ac86cbc1609
Brian See <31948672+seebrian@users.noreply.github.com>
2022-10-04 21:19:06 +0800

    Time: 2126 ms (5.02%), Space: 126.2 MB (21.95%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.java
@@ -2,16 +2,13 @@ class MedianFinder {
     private List<Integer> nums;
     public MedianFinder() {
        nums = new ArrayList<Integer>();
-
     }
-    
     public void addNum(int num) {
         int i = 0;
         for (; i < this.nums.size(); i++) {
             if (this.nums.get(i) >= num) {
                 break;
             }
-            
         }
         this.nums.add(i, num);
     }
@@ -31,3 +28,50 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+/*
+class MedianFinder {
+    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> large = new PriorityQueue<>();
+    private boolean even = true;
+
+    public double findMedian() {
+        if (even)
+            return (small.peek() + large.peek()) / 2.0;
+        else
+            return small.peek();
+    }
+
+    public void addNum(int num) {
+        if (even) {
+            large.offer(num);
+            small.offer(large.poll());
+            
+             System.out.println("even:"+even);
+             System.out.println("num:"+num);
+            for (var e : small) {
+                
+            System.out.println("small.poll:"+e);
+            }
+           for(var l : large) {
+                
+            System.out.println("large.poll:"+l);
+            }
+        } else {
+            small.offer(num);
+            large.offer(small.poll());
+            
+            System.out.println("even:"+even);
+            System.out.println("num:"+num);
+            for(var e : small) {
+                
+            System.out.println("small.poll:"+e);
+            }
+            for(var l : large) {
+                
+            System.out.println("large.poll:"+l);
+            }
+        }
+        even = !even;
+    }
+}
+*/
