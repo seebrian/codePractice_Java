6853067d805b739fe6a03a5937f7dea8b8b6bdcf
Brian See <31948672+seebrian@users.noreply.github.com>
2022-10-07 23:33:12 +0800

    Time: 1 ms (24.07%), Space: 43.3 MB (18.88%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -8,31 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-            
-        return reverse(null, head);
-    }
-    public ListNode reverse (ListNode prev, ListNode curr) {
-        if (curr == null) return prev;
 
-        ListNode tmp = curr.next;
-        curr.next = prev;
-        return reverse (curr, tmp);
-        
+class Solution {
+    //1. Two Pointers
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+       
+        while(curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
     }
 }
+
 // class Solution {
+//     //2. Recursive
 //     public ListNode reverseList(ListNode head) {
-//         ListNode curr = head;
-//         ListNode prev = null;
-       
-//         while(curr != null) {
-//             ListNode tmp = curr.next;
-//             curr.next = prev;
-//             prev = curr;
-//             curr = tmp;
-//         }
-//         return prev;
+            
+//         return reverse(null, head);
+//     }
+//     public ListNode reverse (ListNode prev, ListNode curr) {
+//         if (curr == null) return prev;
+
+//         ListNode tmp = curr.next;
+//         curr.next = prev;
+//         return reverse (curr, tmp);
+        
 //     }
 // }
