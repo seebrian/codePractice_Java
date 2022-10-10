52027635501aa4f1da1cfc4283ccde07c2028206
Brian See <31948672+seebrian@users.noreply.github.com>
2022-10-10 22:14:05 +0800

    Time: 0 ms (100.00%), Space: 41.6 MB (98.67%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -9,23 +9,22 @@
  * }
  */
 class Solution {
-     // Recursive
-     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        head.next = deleteDuplicates(head.next);
-        return head.val == head.next.val ? head.next : head;
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode ptr = head;
+        while (ptr != null && ptr.next != null) {
+            if (ptr.val == ptr.next.val) {
+                ptr.next = ptr.next.next;
+            } else {
+                ptr = ptr.next;
+            }
+        }
+        return head;
     }
-//     public ListNode deleteDuplicates(ListNode head) {
-//         ListNode ptr = head;
-//         while (ptr != null && ptr.next != null) {
-//             if (ptr.val == ptr.next.val) {
-//                 ptr.next = ptr.next.next;
-//             } else {
-//                 ptr = ptr.next;
-//             }
-//         }
-//         return head;
-
-//     }
+    //  // Recursive
+    //  public ListNode deleteDuplicates(ListNode head) {
+    //     if (head == null || head.next == null)
+    //         return head;
+    //     head.next = deleteDuplicates(head.next);
+    //     return head.val == head.next.val ? head.next : head;
+    // }
 }
