8b03725718dba5b70b930b937f133ba5e2260c1b
Brian See <31948672+seebrian@users.noreply.github.com>
2022-10-31 23:07:52 +0800

    Time: 1508 ms (8.28%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT d.name AS Department, A.name AS Employee, A.salary
+FROM 
+	Employee A,
+	Department D   
+WHERE A.DepartmentId = D.Id 
+  AND NOT EXISTS 
+  (SELECT 1 FROM Employee B WHERE B.Salary > A.Salary AND A.DepartmentId = B.DepartmentId) 
